482d50a216bf17736224927774c440103b45d41d
Abhijeet <sabale.abhijeet2@gmail.com>
2022-01-08 09:37:50 +0530

    THis is java branch 08/01/2022

---
+++ src/main/java/javaOfBranch/ToCHeckEverything.java
@@ -0,0 +1,11 @@
+package javaOfBranch;
+
+public class ToCHeckEverything {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello this is new Code");
+
+	}
+
+}
